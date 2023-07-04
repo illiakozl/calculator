a1f97aa0f568ee447fee9ac12e0fadc519f235f7
illia kozlov <samsung.id.od@gmail.com>
2023-07-04 20:34:30 +0200

    first

---
+++ math.js
@@ -0,0 +1,20 @@
+const inputANode = document.querySelector('.js-input-a');
+const inputBNode = document.querySelector('.js-input-b');
+const selectOperationNode = document.querySelector('.js-select-operation');
+const btnresultNode = document.querySelector('.js-btn-result');
+const outputNode = document.querySelector('.js-output');
+
+
+btnresultNode.addEventListener('click' , function() {
+    const a = Number(inputANode.value);
+    const b = Number(inputBNode.value);
+    const operation = selectOperationNode.value;
+
+    const result = calculate({
+        a,
+        b,
+        operation
+    });
+
+    console.log(result);
+})
